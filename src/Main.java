e5bbdc6c1b10ad1aca0a9cd8d05e851aa0654938
Alexey Petrenko <loki2236@mail.ru>
2024-03-26 18:50:38 +0300

    save 24.03.26

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@ import island.Island;
 
 public class Main {
     public static void main(String[] args) {
-        Island island = new Island(100, 100);
+        Island island = new Island(5, 5);
         island.initialIsland();
         island.actionAnimal();
         System.out.println("#".repeat(20));
         island.information();
+        island.endCycle();
     }
 }
--- src/island/Island.java
+++ src/island/Island.java
@@ -1,12 +1,19 @@
 package island;
 
+import entity.Plant;
+import entity.animal.Animal;
 import island.information.GeneralInformation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Island {
     public Location[][] locations;
+
     public Island(int heightLocation, int widthLocation) {
         locations = new Location[heightLocation][widthLocation];
     }
+
     public void initialIsland() {
         for (int i = 0; i < locations.length; i++) {
             for (int j = 0; j < locations[i].length; j++) {
@@ -16,6 +23,7 @@ public class Island {
             }
         }
     }
+
     public void actionAnimal() {
         for (int height = 0; height < locations.length; height++) {
             for (int width = 0; width < locations[height].length; width++) {
@@ -23,7 +31,56 @@ public class Island {
             }
         }
     }
-    public void information(){
+
+    public void checkCycle() {
+        List<Animal> check = new ArrayList<>();
+        for (Location[] location : locations) {
+            for (Location location1 : location) {
+               check = location1.animals.stream().filter(Animal::isEndSpeed).toList();
+            }
+        }
+        if (check.isEmpty()) {
+            // закончить цикл, подсчет и обнуление некоторых характеристик. После подсчета заново запуск локацию
+            System.out.println("End Cycle");
+        }
+    }
+
+    public void endCycle() {
+        for (Location[] location : locations) {
+            for (Location location1 : location) {
+                location1.animals.forEach(animal -> {
+                    animal.setEndSpeed(false);
+                    animal.setQuantitySpeed(animal.getSpeed());
+                    animal.setSaturation(false);
+                });
+            }
+        }
+    }
+
+    public void postPlant(){
+        for (Location[] location : locations) {
+            for (Location value : location) {
+                int number = 200 - value.plants.size();
+                while (number > 0) {
+                    value.plants.add(new Plant());
+                    number--;
+                }
+
+            }
+        }
+    }
+    public void test(){
+        List<Animal> check = new ArrayList<>();
+        for (Location[] location : locations) {
+            for (Location location1 : location) {
+                check = location1.animals.stream().filter(Animal::isSaturation).toList();
+            }
+        }
+        if (check.isEmpty()) {
+            System.out.println("setSaturation(false)");
+        }
+    }
+    public void information() {
         GeneralInformation generalInformation = new GeneralInformation();
         for (Location[] location : locations) {
             for (Location value : location) {
